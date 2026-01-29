505b95c3b4e1389fe493921112ea35eb944d3368
rakshelsinghaniket234 <rakshelsinghaniket234@gmail.com>
2026-01-30 00:44:03 +0530

    rect

---
+++ tilalwind/src/App.jsx
@@ -0,0 +1,11 @@
+import About from "./Component/About"
+function App(){
+  return(
+    <>
+    <About/>
+    <h1 className="text-2xl p-3 bg-amber-400">this is my app</h1>
+
+    </>
+  )
+}
+export default App
---
+++ tilalwind/src/Component/About.jsx
@@ -0,0 +1,24 @@
+
+function About(){
+    return(
+      
+        <div className="flex">
+          
+  <div>
+    <p className="text-4xl font-mono bg-lime-200">"My All Time Favorite Cricketer is Virat Kohli"</p> <br/>
+    <hr />
+    <h2 className="text-6xl font-sans bg-orange-600" >Virat Kohli Is The Best All Formate Bastman In World</h2><br/>
+    <h1 className="text-8xl font-serif bg-blue-400">The Run Machine</h1> <br/>
+    <h1 className="text-9xl bg-amber-800">KING VIRAT KOHLI </h1>
+  </div>
+
+  <div className="size-150">
+    <img src="fan18.jpg" alt="Developer" />
+   </div>
+
+</div>
+            
+       
+    )
+}
+export default About
